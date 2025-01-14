2c4683e3d2df1c071d316e9ffb3a5d143428b7ff
omaraguilarr84 <omar.hawks8@gmail.com>
2025-01-14 12:51:02 -0500

    added avo TL before

---
+++ AvoA/Scripts/Avo_TL_Before.m
@@ -0,0 +1,48 @@
+clear; clc; close all;
+
+dataFolder = '../Data/';
+
+subfolders = dir(dataFolder);
+subfolders = subfolders([subfolders.isdir]);
+subfolders = subfolders(~ismember({subfolders.name}, {'.', '..'}));
+
+dates = {subfolders.name};
+dates = sort(dates);
+
+fixedDate = dates{1};
+fixedPath = fullfile(dataFolder, fixedDate, 'series');
+[fixedImage, fInfo] = loadDicom3D(fixedPath);
+
+threshold_shell = 500;
+fixedImageBW = double(fixedImage > threshold_shell);
+
+shellFixed = find(any(any(fixedImageBW > 0, 1), 2));
+
+% If image z-dimension is backwards, reverse the image
+if shellFixed(1) < 300
+    fixedImage = fixedImage(:, :, end:-1:1);
+end
+
+fixedMIP = MIPxyz(fixedImage, false);
+MIPImages = fixedMIP.tile;
+
+for i = 2:length(dates)
+    fprintf('Creating MIP %d of %d...\n', i-1, length(dates)-1);
+
+    movingDate = dates{i};
+    movingPath = fullfile(dataFolder, movingDate, 'series');
+    [movingImage, mInfo] = loadDicom3D(movingPath);
+
+    movingImageBW = double(movingImage > threshold_shell);
+    
+    shellMoving = find(any(any(movingImageBW > 0, 1), 2));
+
+    if shellMoving(1) < 300
+        movingImageBW = movingImageBW(:, :, end:-1:1);
+    end
+
+    movingMIP = MIPxyz(movingImage, false);
+    MIPImages = cat(3, MIPImages, movingMIP.tile);
+end
+
+MIPxyzLapse(MIPImages);
